f61286f8348236a20444572f57ccc95dadcc6cc6
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-01 19:06:15 +0530

    compare_ab.py

--- cspp1-practice/m3/compare_AB.py
+++ cspp1-practice/m3/compare_AB.py
@@ -4,7 +4,7 @@ print the message
 '''
 VARA = input()
 VARB = input()
-if type(VARA) == str or type(VARB) == str:
+if isinstance(VARA) == str or isinstance(VARB) == str:
     print("string involved")
 elif VARA > VARB:
     print("bigger")

06a951e84d589cdd19542d1cf998cfc6294667ad
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-02 14:48:17 +0530

    " -> 0 of 1 passed. pylint: 6.00/10 "

--- cspp1-practice/m3/sum_1_end.py
+++ cspp1-practice/m3/sum_1_end.py
@@ -1,9 +1,15 @@
 '''
 @author :Arepallyrakesh
-prints sum 1 end for numbers
+prints sum 1 end numbers
 '''
 S = 0
 N = 1
+while N <= 6:
+    S = S + N
+    N = N + 1
+print(S)
+
 for N in range(1, 7, 1):
     S = S + N
-print(S)
+    print(S)
+    

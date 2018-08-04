d944b7fd32561d9efa247e429d3e292dc471e767
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-04 12:42:04 +0530

    " -> 6 of 7 passed. pylint: 1.11/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -8,14 +8,19 @@ def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
-    int_input = (input())
-    st = str(int_input)
-    l = len(int_input)
-    s=0  
-    for i in range(l):
-    	while i <= l:
-    		s = s *st[i]
-    		print(s)
+    n = (input())
+    la = list(str(n))
+    s = 1
+    for i in str(n):
+    	s = s * int(i)
+    print(s)
+
+
+
+
+
+
+
 
     
 

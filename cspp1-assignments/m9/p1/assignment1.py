1ddfd56c0918080f9dbb3dee8e07946836714e13
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-08 17:30:54 +0530

    " -> 3 of 9 passed. pylint: 8.42/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -17,7 +17,7 @@ def is_word_guessed(secret_word, letters_guessed):
     # FILL IN YOUR CODE HERE...
     for i in range(len(secret_word)):
         j = 1
-        if secret_word[i] == letters_guessed[j]:
+        if secret_word[i] in letters_guessed[j]:
             return True
         else:
             return False

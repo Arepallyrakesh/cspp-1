e3847801519e907a01a475c2a078fd20503fc244
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-12 23:26:29 +0530

    " -> 6 of 6 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p2/p2/assignment2.py
+++ cspp1-assignments/m11/p2/p2/assignment2.py
@@ -6,17 +6,17 @@ has no side effects: i.e., it must not mutate the hand passed
 in. Before pasting your function definition here,
 be sure you've passed the appropriate tests in test_ps4a.py.
 '''
-def updateHand(hand, word):
+def update_hand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
     Has no side effects: does not modify hand.
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
@@ -30,11 +30,11 @@ def main():
     '''
     n_i = input()
     adict = {}
-    for i in range(int(n_i)):
+    for data in range(int(n_i)):
         data = input()
         l_ist = data.split()
         adict[l_ist[0]] = int(l_ist[1])
     data1 = input()
-    print(updateHand(adict,data1))
+    print(update_hand(adict, data1))
 if __name__ == "__main__":
     main()

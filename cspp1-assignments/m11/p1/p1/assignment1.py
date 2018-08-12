a83c3d3a0989d71cec8bfef2b4908ea2b5789895
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-12 23:14:46 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p1/p1/assignment1.py
+++ cspp1-assignments/m11/p1/p1/assignment1.py
@@ -8,7 +8,7 @@ a single word. The function get_word_score should
   and return the integer score for that word, using
   the game's scoring rules.
 '''
-scrabble_letter_val = {
+SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8,
     'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
     'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10,
@@ -29,7 +29,7 @@ def get_word_score(word, n_l):
     # TO DO ... <-- Remove this comment when you code this function
     c_t = 0
     for i in word:
-        c_t += scrabble_letter_val[i]
+        c_t += SCRABBLE_LETTER_VALUES[i]
     if len(word) == n_l:
         return c_t * len(word) + 50
     return c_t*len(word)

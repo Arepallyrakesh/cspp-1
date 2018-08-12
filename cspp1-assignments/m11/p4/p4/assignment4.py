3ff29fe38921934f2f9ad87a5b8599071a8144df
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-12 23:50:09 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p4/p4/assignment4.py
+++ cspp1-assignments/m11/p4/p4/assignment4.py
@@ -7,12 +7,12 @@ the playHand function. This function allows the user to
 play out a single hand. First, though, you'll need to
 implement the helper calculateHandlen function,
 which can be done in under five lines of code.
-''' 
+'''
 
 def calculate_handlen(hand):
-    """ 
+    """
     Returns the length (number of letters) in the current hand.
-    
+
     hand: dictionary (string int)
     returns: integer
     """
@@ -23,15 +23,15 @@ def calculate_handlen(hand):
     return count
 
 def main():
-    ''' 
+    '''
     main function
-    ''' 
-    n_input=input()
-    a_dict={}
+    '''
+    n_input = input()
+    a_dict = {}
     for data in range(int(n_input)):
-        data=input()
-        l_list=data.split()
-        a_dict[l_list[0]]=int(l_list[1])
+        data = input()
+        l_list = data.split()
+        a_dict[l_list[0]] = int(l_list[1])
     print(calculate_handlen(a_dict))
 
 

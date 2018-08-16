479d928588c774b04e99a6516210243ef1e6fd2f
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-16 09:46:02 +0530

    " -> 1 of 3 passed. pylint: 9.03/10 "

--- cspp1-practice/m14/CodeCampPoker/poker.py
+++ cspp1-practice/m14/CodeCampPoker/poker.py
@@ -64,7 +64,7 @@ def hand_rank(hand):
         count = 6 
     elif is_flush(hand):
         count = 5
-    else is_straight(hand):
+    elif is_straight(hand):
         count = 0
     return count
 

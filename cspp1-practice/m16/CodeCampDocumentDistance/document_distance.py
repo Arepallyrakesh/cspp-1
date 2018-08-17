e9968504e8391513c282ae1837b771367f0ca895
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-17 21:38:20 +0530

    " -> 0 of 3 passed. pylint: 7.45/10 "

--- cspp1-practice/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/m16/CodeCampDocumentDistance/document_distance.py
@@ -7,13 +7,13 @@ def combine_dictonary(dictionary_one, dictionary_two):
     dictionary = {}
     for word in dictionary_one:
         if word in dictionary_two:
-            dictionary[word] = [dictonary_one[word], dictonary_two[word]]
+            dictionary[word] = [dictionary_one[word], dictionary_two[word]]
     for word in dictionary_one:
         if word not in dictionary:
-            dictionary[word] = [dictonary_one[word], 0]
+            dictionary[word] = [dictionary_one[word], 0]
     for word in dictionary_two:
         if word not in dictionary:
-            dictionary[word] = [0, dictonary_two[word]]
+            dictionary[word] = [0, dictionary_two[word]]
     return dictionary
 
 def clean_given_text(text_input):

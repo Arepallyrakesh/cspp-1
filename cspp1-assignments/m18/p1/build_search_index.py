25f7cd5f5d18fa59c800cb6c51071b5562eefd6b
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-18 17:22:28 +0530

    " -> 0 of 1 passed. pylint: 8.70/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -49,7 +49,6 @@ def word_list(text):
     regex = re.compile('[^a-z]')
     list1 = regex.sub(" ", string1).split(" ")
     return list1
-    print(list1)
 
 def build_search_index(docs):
     '''
@@ -70,7 +69,7 @@ def build_search_index(docs):
     # document_1 = word_list(docs)
     # print(document_1)
     search_index = {}
-    print(docs)
+    
     # print(document_1)
     for ind, word in enumerate(docs):
         list1 = remove_swords(word_list(word))

d0bb31d5d8e43c98baa2cf84ddfca2debdd9143d
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-18 11:22:18 +0530

    " -> 0 of 1 passed. pylint: 8.25/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -56,7 +56,7 @@ def word_list(text):
     removetable = str.maketrans('', '', '@#%123456789')
     x_lis = [s.translate(removetable) for s in text]
     string = ''.join(x_lis)
-    string1 = string.lower()
+    string1 = string.lower().strip()
     regex = re.compile('[^a-z]')
     list1 = regex.sub(" ", string1).split(" ")
     return list1
@@ -84,7 +84,7 @@ def build_search_index(docs):
     dictionary = {}
     stopwords = load_stopwords("stopwords.txt")
     for word in enumerate(document_1):
-        word = word.strip()
+        # word = word.strip()
         if word not in stopwords and len(word) > 0:
             if word  in dictionary:
                 dictionary[word]  = 1

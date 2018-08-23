ef17afea5f38c73ecd69a72cd518e978c181bf58
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-23 14:50:16 +0530

    " -> 1 of 8 passed. pylint: 3.55/10 "

--- cspp1-practice/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practice/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -9,12 +9,12 @@ def mult_matrix(m1, m2):
      
     result1 = []
     for i in range(len(m1)):
+        result = []
         for j in range(len(m2[0])):
-            result = []
-            for k in range(len(m2)):
-                result.append(m1[i][k] * m2[k][j])
+                result.append(m1[i][j] * m2[j][i])
+            # for k in range(len(m2)):
         result1.append(result)
-    return result
+    return result1
   
 
 

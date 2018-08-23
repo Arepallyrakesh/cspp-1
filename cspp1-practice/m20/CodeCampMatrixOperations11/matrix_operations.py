24866aff0bd313200776ece5cc83abfa214a32cc
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-23 20:28:17 +0530

    " -> 8 of 8 passed. pylint: 5.68/10 "

--- cspp1-practice/m20/CodeCampMatrixOperations11/matrix_operations.py
+++ cspp1-practice/m20/CodeCampMatrixOperations11/matrix_operations.py
@@ -1,7 +1,7 @@
 '''
 @author: Arepallyrakesh
 '''
-def mult_matrix(m_1	, m_2):
+def mult_matrix(m_1 , m_2):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -12,20 +12,20 @@ def mult_matrix(m_1	, m_2):
 
     # result2 = []
 
-    # result = [[0 for row in range(len(m_1	))] for col in range(len(m_2[0]))]
-    # for i in range(len(m_1	)):
+    # result = [[0 for row in range(len(m_1 ))] for col in range(len(m_2[0]))]
+    # for i in range(len(m_1    )):
     #     for j in range(len(m_2[0])):
     #         for k in range(len(m_2)):
-    #             result[i][j] += m_1	[i][k] * m_2[k][j]
+    #             result[i][j] += m_1   [i][k] * m_2[k][j]
     #         # result1.append(result)
     #     # result1.append(result1)
     # return result
     add_ = re_mat(len(m_1), len(m_2[0]))
-    if len(m_1	[0]) == len(m_2):
+    if len(m_1  [0]) == len(m_2):
         for i in range(len(m_1)):
             for j in range(len(m_2[0])):
                 for k in range(len(m_2)):
-                    add_[i][j] += m_1	[i][k] * m_2[k][j]
+                    add_[i][j] += m_1   [i][k] * m_2[k][j]
         return add_
     else:
         print("Error: Matrix shapes invalid for mult")
@@ -37,15 +37,15 @@ def mult_matrix(m_1	, m_2):
 
 
 def re_mat(rows, colums):
-	'''
-	empty matrix
-	'''
+    '''
+    empty matrix
+    '''
     add_matrix = [[0 for i in range(colums)] for j in range(rows)]
     return add_matrix
 
 
 
-def add_matrix(m_1	, m_2):
+def add_matrix(m_1  , m_2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -53,27 +53,27 @@ def add_matrix(m_1	, m_2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    # if len(m_1	) == len(m_2) and len(m_1	[0]) == len(m_2[0]):
+    # if len(m_1    ) == len(m_2) and len(m_1   [0]) == len(m_2[0]):
     #     result1 = []
-    #     for i in range(len(m_1	)):
+    #     for i in range(len(m_1    )):
     #         result = []
-    #         for j in range(len(m_1	[0])):
-    #             result.append(m_1	[i][j] + m_2[i][j])
+    #         for j in range(len(m_1    [0])):
+    #             result.append(m_1 [i][j] + m_2[i][j])
     #             # result1.append(list(map(int,  )))
     #         result1.append(result)
     #     return result1
     # # except ValueError:
     # #     print("Matrix shapes invalid for add")
-    # if len(m_1	) == len(m_2) and len(m_1	[0]) == len(m_2[0]):
-    # print(m_1	, m_2)
+    # if len(m_1    ) == len(m_2) and len(m_1   [0]) == len(m_2[0]):
+    # print(m_1 , m_2)
     # add_ = []
-    # print(m_1	, m_2)
-    add_ = re_mat(len(m_1	), len(m_1	[0]))
-    if len(m_1	) == len(m_2) and len(m_1	[0]) == len(m_2[0]):
-        for i in range(len(m_1	)):
-            for j in range(len(m_1	[0])):
+    # print(m_1 , m_2)
+    add_ = re_mat(len(m_1   ), len(m_1  [0]))
+    if len(m_1  ) == len(m_2) and len(m_1   [0]) == len(m_2[0]):
+        for i in range(len(m_1  )):
+            for j in range(len(m_1  [0])):
                 # add_1 = add_
-                add_[i][j] += (m_1	[i][j] + m_2[i][j])
+                add_[i][j] += (m_1  [i][j] + m_2[i][j])
         return add_
     else:
         print("Error: Matrix shapes invalid for addition")
@@ -113,8 +113,8 @@ def read_matrix():
     return matrix
 def main():
     # read matrix 1
-    m_1	 = read_matrix()
-    if m_1	 is None:
+    m_1  = read_matrix()
+    if m_1   is None:
         exit()
 
     # read matrix 2

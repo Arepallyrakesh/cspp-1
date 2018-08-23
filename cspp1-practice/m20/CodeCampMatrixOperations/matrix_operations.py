166e895ca0e5d9d72f362c224c5285464db5909a
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-23 17:26:47 +0530

    " -> 0 of 8 passed. pylint: 1.94/10 "

--- cspp1-practice/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-practice/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -44,7 +44,7 @@ def add_matrix(m1, m2):
     #     return result1
     # # except ValueError:
     # #     print("Matrix shapes invalid for add")
-     if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
         for i in range(len(m1)):
             for j in range(len(m1[0])):
                 add = re_mat(len(m1), len(m1[0]))

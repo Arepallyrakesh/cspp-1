dfcf4f9577265fe9d6f26a8b3dc7a558d773dcd9
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-24 12:48:14 +0530

    " -> 0 of 14 passed. pylint: -21.88/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -20,8 +20,33 @@ def is_validation(m1):
     
 # def string1(m1):
 #     print(list(itertools.chain(*m1)))
+def result_int(cnt):
+    for i in m1:
+        if m1[0][0] == m1[1][1] == m1[0][1]:
+            return cnt
+            exit()
 
-
+        elif m1[0][0] == m1[0][1] == m1[0][2]:
+            return cnt
+            exit()
+        elif m1[1][0] == m1[1][1] == m1[2][1]:
+            return cnt
+            exit()
+        elif m1[2][0] == m1[2][1] == m1[2][2]:
+            return cnt
+            exit()
+        elif m1[0][0] == m1[1][0] == m1[2][0]:
+            return cnt
+            exit()
+        elif m1[0][1] == m1[1][1] == m1[2][1]:
+            return cnt
+            exit()
+        elif m1[0][2] == m1[1][2] == m1[2][2]:
+            return cnt
+            exit()
+        elif m1[2][0] == m1[1][1] == m1[0][2]:
+            return cnt
+            exit()
 def read_input():
     matrix = []
     for i in range(3):
@@ -32,6 +57,6 @@ def read_input():
 
 def main():
     m1 = read_input()
-    print(is_validation(m1))
+    print(result_int(is_validation(m1)))
 
 main()

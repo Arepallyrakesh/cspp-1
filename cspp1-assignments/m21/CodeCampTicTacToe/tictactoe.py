4107e9637bed032071252999126f29abeb749028
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-24 17:16:11 +0530

    " -> 11 of 14 passed. pylint: 5.32/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -13,7 +13,7 @@ def count_mat(m1, cnt):
 def is_validation(m1):  
     for i in m1:
         for j in i:
-            if j not  in ['x','o', '.']:
+            if j not  in 'xo.':
                 print("invalid input")
                 return False
     if (count_mat(m1, 'x') > 5) or (count_mat(m1, 'o') > 5) or count_mat(m1, 'x') == count_mat(m1, 'o'):
@@ -21,7 +21,8 @@ def is_validation(m1):
         return False
     return True
     if (count_mat(m1, 'x') == 4 and count_mat(m1, 'o') ==5)  or (count_mat(m1, 'x') == 5 and count_mat(m1, 'o') == 4):
-        return "draw"
+        print("draw")
+        return False
 
 # def is_draw()
 # def string1(m1):

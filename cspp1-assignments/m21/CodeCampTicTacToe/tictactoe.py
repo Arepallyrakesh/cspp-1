c71adb34113316ebac5c478c331c076118afee5d
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-24 13:23:07 +0530

    " -> 3 of 14 passed. pylint: -1.78/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -19,7 +19,7 @@ def is_validation(m1):
             #     return 'x'
             # elif j is 'o':
             #     return 'o
-            if j is 'xo':
+            if j in 'xo':
                 return j 
              
     # if (count_mat(m1, 'x') >= 5) or (count_mat(m1, 'o') >= 5) or count_mat(m1, 'x') == count_mat(m1, 'o'):

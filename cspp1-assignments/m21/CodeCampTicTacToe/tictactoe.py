4396bd955b011cfb4632cd2d026e485495a94062
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-24 18:41:04 +0530

    " -> 14 of 14 passed. pylint: 8.94/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -1,52 +1,52 @@
 '''
 @author:Arepallyrakesh
 '''
-def count_mat(m1, cnt):
+def count_mat(m_1, cnt_var):
     '''
     counting the values
     '''
     count1 = 0
-    for word in m1:
-        count1 += word.count(cnt)
+    for word in m_1:
+        count1 += word.count(cnt_var)
     return count1
-def is_validation(m1):
+def is_validation(m_1):
     '''
     validation of matrices
     '''
-    for i in m1:
+    for i in m_1:
         for j in i:
             if j not  in 'xo.':
                 print("invalid input")
                 return False
-    if (count_mat(m1, 'x') == 4 and count_mat(m1, 'o') ==5)  or \
-     (count_mat(m1, 'x') == 5 and count_mat(m1, 'o') == 4):
+    if (count_mat(m_1, 'x') == 4 and count_mat(m_1, 'o') ==5)  or \
+     (count_mat(m_1, 'x') == 5 and count_mat(m_1, 'o') == 4):
         print("draw")
         return False
-    if (count_mat(m1, 'x') > 5) or (count_mat(m1, 'o') > 5) or\
-     count_mat(m1, 'x') == count_mat(m1, 'o'):
+    if (count_mat(m_1, 'x') > 5) or (count_mat(m_1, 'o') > 5) or\
+     count_mat(m_1, 'x') == count_mat(m_1, 'o'):
         print("invalid game")
         return False
     return True
-def result_int(m1):
+def result_int(m_1):
     '''
     result of input
     '''
-    if m1[0][0] == m1[1][1] == m1[2][2]:
-        return m1[0][0]
-    elif m1[0][0] == m1[0][1] == m1[0][2]:
-        return m1[0][1]
-    elif m1[1][0] == m1[1][1] == m1[1][2]:
-        return m1[1][0]
-    elif m1[2][0] == m1[2][1] == m1[2][2]:
-        return m1[2][0]
-    elif m1[0][0] == m1[1][0] == m1[2][0]:
-        return m1[0][0]
-    elif m1[0][1] == m1[1][1] == m1[2][1]:
-        return m1[0][1]
-    elif m1[0][2] == m1[1][2] == m1[2][2]:
-        return m1[0][2]
-    elif m1[2][0] == m1[1][1] == m1[0][2]:
-        return m1[2][0]
+    if m_1[0][0] == m_1[1][1] == m_1[2][2]:
+        return m_1[0][0]
+    elif m_1[0][0] == m_1[0][1] == m_1[0][2]:
+        return m_1[0][1]
+    elif m_1[1][0] == m_1[1][1] == m_1[1][2]:
+        return m_1[1][0]
+    elif m_1[2][0] == m_1[2][1] == m_1[2][2]:
+        return m_1[2][0]
+    elif m_1[0][0] == m_1[1][0] == m_1[2][0]:
+        return m_1[0][0]
+    elif m_1[0][1] == m_1[1][1] == m_1[2][1]:
+        return m_1[0][1]
+    elif m_1[0][2] == m_1[1][2] == m_1[2][2]:
+        return m_1[0][2]
+    elif m_1[2][0] == m_1[1][1] == m_1[0][2]:
+        return m_1[2][0]
     else:
         return ("invalid input")
 def read_input():
@@ -62,7 +62,7 @@ def main():
     '''
     main function
     '''
-    m1 = read_input()
-    if is_validation(m1):
-        print(result_int(m1))
+    m_1 = read_input()
+    if is_validation(m_1):
+        print(result_int(m_1))
 main()

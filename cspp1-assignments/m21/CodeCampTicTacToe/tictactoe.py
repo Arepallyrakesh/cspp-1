4f2f288b05a878456b6ca4ce5aea885804ad168d
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-24 13:53:53 +0530

    " -> 3 of 14 passed. pylint: 0.48/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tictactoe.py
@@ -13,7 +13,8 @@ def count_mat(m1, cnt):
 def is_validation(m1):
     if (count_mat(m1, 'x') >= 5) or (count_mat(m1, 'o') >= 5) or count_mat(m1, 'x') == count_mat(m1, 'o'):
         return "invalid game"
-    break
+        exit()
+
     for i in m1:
         for j in i:
             if j not  in 'xo.':

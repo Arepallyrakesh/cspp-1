ae4da2cdd64e1a96186ec48cee6e908bf8a1ae2d
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-25 11:14:43 +0530

    " -> 3 of 3 passed. pylint: 6.00/10 "

--- cspp1-assignments/m22/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -4,18 +4,12 @@ alphabets in both upper and lower case and numbers.
 '''
 import re
 def clean_string(string):
-	'''
-	clean the string
-	'''
 	
     regex = re.compile('[^a-z0-9]+')
     words = regex.sub("", string)
     return words
 
 def main():
-	'''
-	main function
-	'''
     string = input()
     print(clean_string(string))
 

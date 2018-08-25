99be51eb5aa66d81735bdf9c19079fba1ff4c8f8
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-25 11:12:20 +0530

    " -> 1 of 3 passed. pylint: 6.00/10 "

---
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -0,0 +1,17 @@
+'''
+Write a function to clean up a given string by removing the special characters and retain 
+alphabets in both upper and lower case and numbers.
+'''
+import re
+def clean_string(string):
+	
+    regex = re.compile('[^a-z0-9]+')
+    words = regex.sub(" ", string)
+    return words
+
+def main():
+    string = input()
+    print(clean_string(string))
+
+if __name__ == '__main__':
+    main()

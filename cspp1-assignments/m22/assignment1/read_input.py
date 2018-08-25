ca60199c621484d28007b832a39f8a9b26dde4b4
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-25 11:02:38 +0530

    " -> 2 of 2 passed. pylint: 8.33/10 "

--- cspp1-assignments/m22/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/read_input.py
@@ -2,32 +2,17 @@
 Write a python program to read multiple lines of text input and store the input into a string.
 '''
 def read_input():
-	input1 = int(input())
-	list1 = []
-	for i in range(input1):
-		input_str = input()
-		list1.append(input_str)
-		text = '\n'.join(list1)
-	return text 
-
-
+    '''
+    read input
+    '''
+    input1 = int(input())
+    list1 = []
+    for _ in range(input1):
+        input_str = input()
+        list1.append(input_str)
+        text = '\n'.join(list1)
+    return text 
 def main():
-	print(read_input())
-    
-
+    print(read_input())
 if __name__ == '__main__':
     main()
-
-
-# matrix = []
-#     list_input = input().split(",")
-#     rows, columns = int(list_input[0]), int(list_input[1])
-#     for _ in range(rows):
-#         list_mat_row = input().split()
-#         if columns == len(list_mat_row):
-#             matrix.append([int(i) for i in list_mat_row])
-#             # print(matrix)
-#         else:
-#             print("Error: Invalid input for the matrix")
-#             return None
-#     return matrix

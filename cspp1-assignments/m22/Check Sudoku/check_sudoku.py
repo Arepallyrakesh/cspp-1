5b3a86d429198296d182efdc563d74ab969a42b7
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-25 13:23:45 +0530

    " -> 15 of 26 passed. pylint: 4.17/10 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -7,22 +7,38 @@
     Complete the check_sudoku function to check if the given grid
     satisfies all the sudoku rules given in the statement above.
 '''
+def row_sudoku(sudoku):
+    for value in sudoku:
+        if len(set(value)) != 9:
+            return False
+    return True
+def col_sudoku(sudoku):
+    """checks for valid coloumn"""
+    list1 = []
+    for value in range(len(sudoku)):
+        row = []
+        for j in range(len(sudoku[0])):
+            row.append(sudoku[i][j])
+        list1.append(row)
+        value += 1
+    return rows(list1)
 
 def check_sudoku(sudoku):
     '''
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    list1 = []
-    for i in sudoku:
-        if len(set(i)) == 9:
-        for i in range(len(sudoku)):
-            row = []
-            for j in range(len(sudoku[0])):
-                row.append(sudoku[j][i])
-                list1.append(row)
-                i += 1
-    return list1
+    # list1 = []
+    # for i in sudoku:
+    #     if len(set(i)) == 9:
+    #     for i in range(len(sudoku)):
+    #         row = []
+    #         for j in range(len(sudoku[0])):
+    #             row.append(sudoku[j][i])
+    #             list1.append(row)
+    #             i += 1
+    # return list1
+    return row_sudoku(sudoku) and col_sudoku(sudoku)
 
 
 

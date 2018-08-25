fc70faa270992c2aebcee00f6d1172392ff34173
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-08-25 11:58:03 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -4,25 +4,26 @@ each word
 '''
 
 def tokenize(string, input1):
-    list1 = []
-    counts = dict()
-    for _ in range(input1):
-        input_str = input()
-        list1.append(input_str)
-        text = '\n'.join(list1)
-        for word in text:
-            if word in counts:
-                counts[word] += 1
-            else:
-                counts[word] = 1
+    # list1 = []
+    # counts = dict()
+    # for _ in range(input1):
+    #     input_str = input()
+    #     list1.append(input_str)
+    #     text = '\n'.join(list1)
+    #     for word in text:
+    #         if word in counts:
+    #             counts[word] += 1
+    #         else:
+    #             counts[word] = 1
 
-    return counts
+    # return counts
     
             
 def main():
     input1 = int(input())
     input_str = input()
-    print(tokenize(input_str, input1))
+    print(input_str)
+    # print(tokenize(input_str, input1))
 if __name__ == '__main__':
     main()
 
